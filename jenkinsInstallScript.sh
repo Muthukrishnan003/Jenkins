aad981a8b73ad36471ed128756b36c8c23be3230
muthukrishnan <mkmuthukrishnan003@gmail.com>
2024-06-02 18:35:41 +0530

    adding file and directory to jenkins

---
+++ jenkinsInstallScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt update -y
+
+# java installation
+apt install openjdk-17-jdk -y
+
+# jenkins installation of long term support
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+# apt install apache2 -y
+
+systemctl status jenkins
+
